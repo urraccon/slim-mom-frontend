a2bfe1ce46f0cc11f7d9cbe0556d3a6e77d79e18
urraccon <133591634+urraccon@users.noreply.github.com>
2025-04-24 15:06:06 +0300

    updated API URL

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://slim-mom-api-b3344cba0d98.herokuapp.com/api",
     credentials: "include",
   }),
   endpoints: () => ({}),
