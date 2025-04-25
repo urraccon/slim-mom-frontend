8bda625a0462ba7ceec1d4810acb9273e5862b03
urraccon <133591634+urraccon@users.noreply.github.com>
2025-04-25 09:52:36 +0300

    added .env.production file

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = import.meta.env.VITE_API_URL;
+
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://slimmom-api-2psx.onrender.com/api",
+    baseUrl,
     credentials: "include",
   }),
   endpoints: () => ({}),
